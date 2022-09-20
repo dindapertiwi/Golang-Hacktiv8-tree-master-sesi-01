59518b92ba2f334c3bb9b60f6e0473f0fc99d50c
dindapertiwi <113920687+dindapertiwi@users.noreply.github.com>
2022-09-21 03:05:26 +0700

    Add files via upload

---
+++ hello.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// belajar hello
+	fmt.Println("Hello")
+	fmt.Print("Dinda", "Ayu", "Hikmah", "Pertiwi", "Belajar Ngoding")
+
+	//
+	fmt.Println("Hello", "Dinda")
+	fmt.Print("Hello Dinda\n")
+	fmt.Print("Hello", " Dinda\n")
+	fmt.Print("Hello", " ", "Dinda\n")
+}
